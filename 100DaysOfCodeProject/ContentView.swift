233406bbc718a9ee1251480193d8081b3c83ca97
Malik Hawkins <hawkinsjm94@gmail.com>
2020-05-27 12:30:20 -0400

    Initial Commit

---
+++ 100DaysOfCodeProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  100DaysOfCodeProject
+//
+//  Created by Malik Hawkins on 5/27/20.
+//  Copyright © 2020 Malik Hawkins. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
