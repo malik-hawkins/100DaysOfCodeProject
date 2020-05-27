db81ea6acd98062a1e978913de67fedee992fe3a
Malik Hawkins <hawkinsjm94@gmail.com>
2020-05-27 15:49:49 -0400

    feat(access): added navigation for the user/admin access screens.

--- 100DaysOfCodeProject/ForgotPassword.swift
+++ 100DaysOfCodeProject/ForgotPassword.swift
@@ -10,64 +10,48 @@ import SwiftUI
 
 struct ForgotPassword: View {
     
-        @State private var email: String = ""
-        @State private var password: String = ""
-        
-        var body: some View {
-            GeometryReader{ geo in
-                VStack(alignment: .center, spacing: 20){
-                    VStack(alignment: .center){
-                        Text("Forgot Password ?")
-                            .font(.title)
-                            .fontWeight(.bold)
-                    }.padding(.top,100).padding(.bottom, 20)
-                    Text("Please enter the email address \nassociated with your account!")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.bottom, 20)
-                    VStack(alignment: .trailing){
-                        TextField("Enter Email Address", text: self.$email)
-                            .padding(.all)
-                            .frame(height:50)
-                            .background(Color.init(white: 0.95))
-                            .cornerRadius(6)
-                            .font(.system(size: 13, weight:.bold))
-                        
-                        Spacer()
-    //                    HStack(alignment: .center) {
-    //                        Button(action: {
-    //                            print("Implement back button")
-    //                        }){
-    //                            Text("Back")
-    //                        }
-                            Spacer()
-    //                        Button(action: {
-    //                            print("Implement next button")
-    //                        }) {
-    //                            Text("Next")
-    //                                .font(.system(size: 15, weight:.bold))
-    //                                .frame(minWidth: 0, maxWidth: 100, minHeight: 45)
-    //                                .background(Color.init(red: 14/255, green: 219/255, blue: 139/255))
-    //                                .foregroundColor(Color.white)
-    //                                .cornerRadius(6)
-    //                        }
-    //                    }
-                    }.padding(.leading,40).padding(.trailing,40)
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        GeometryReader{ geo in
+            VStack(alignment: .center, spacing: 20){
+                VStack(alignment: .center){
+                    Text("Forgot Password ?")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }.padding(.top,100).padding(.bottom, 20)
+                Text("Please enter the email address \nassociated with your account!")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .padding(.bottom, 20)
+                VStack(alignment: .trailing){
+                    TextField("Enter Email Address", text: self.$email)
+                        .padding(.all)
+                        .frame(height:50)
+                        .background(Color.init(white: 0.95))
+                        .cornerRadius(6)
+                        .font(.system(size: 13, weight:.bold))
                     
-                    Button(action: {
-                        print("Implement next button")
-                    }) {
-                        Text("Next")
-                            .font(.system(size: 15, weight:.bold))
-                            .frame(minWidth: 0, maxWidth: .greatestFiniteMagnitude, minHeight: 45)
-                            .background(Color.init(red: 14/255, green: 219/255, blue: 139/255))
-                            .foregroundColor(Color.white)
-                            .cornerRadius(6)
-                    }.padding(.horizontal)
-                    
-                }
+                    Spacer()
+                    Spacer()
+ 
+                }.padding(.leading,40).padding(.trailing,40)
+                
+                Button(action: {
+                    print("Implement next button")
+                }) {
+                    Text("Next")
+                        .font(.system(size: 15, weight:.bold))
+                        .frame(minWidth: 0, maxWidth: .greatestFiniteMagnitude, minHeight: 45)
+                        .background(Color.init(red: 14/255, green: 219/255, blue: 139/255))
+                        .foregroundColor(Color.white)
+                        .cornerRadius(6)
+                }.padding(.horizontal)
+                
             }
         }
+    }
     
 }
 
--- 100DaysOfCodeProject/Welcome.swift
+++ 100DaysOfCodeProject/Welcome.swift
@@ -9,66 +9,82 @@
 import SwiftUI
 
 struct Welcome: View {
-   
-         @State private var username: String = ""
-           @State private var password: String = ""
-           
-           var body: some View {
-               GeometryReader{ geo in
-                   VStack(alignment: .leading, spacing:50){
-                       VStack(alignment:.leading){
-                           Text("Welcome,")
-                               .font(.largeTitle)
-                               .fontWeight(.bold)
-                           Text("sign in to continue!")
-                               .font(.title)
-                               .foregroundColor(Color.gray)
-                               .fontWeight(.semibold)
-                       }.padding(.top,100).padding(.leading,40)
-                       VStack(alignment: .trailing){
-                           TextField("Enter Username", text: self.$username)
-                               .padding(.all)
-                               .frame(height:50)
-                               .background(Color.init(white: 0.95))
-                               .cornerRadius(6)
-                               .font(.system(size: 13, weight:.bold))
-                           SecureField("Enter Password", text: self.$password)                  .padding(.all)
-                               .frame(height:50)
-                               .background(Color.init(white: 0.95))
-                               .cornerRadius(6)
-                               .font(.system(size: 13, weight:.bold)).foregroundColor(Color.primary)
-                           Button(action: {
-                               print("Implement Forgot Password!")
-                           }) {
-                               Text("Forgot Password?").font(.system(size: 14)).fontWeight(.semibold)
-                           }
-                           Spacer()
-                                
-                               HStack(alignment: .center) {
-                                   Button(action: {
-                                       print("Implement login!")
-                                   }) {
-                                       Text("Sign Up")
-                                   }
-                                   
-                                   Spacer()
-                                   Button(action: {
-                                       print("Implement next button")
-                                   }) {
-                                       Text("Next")
-                                           .font(.system(size: 15, weight:.bold))
-                                           .frame(minWidth: 0, maxWidth: 100, minHeight: 45)
-                                           .background(Color.init(red: 14/255, green: 219/255, blue: 139/255))
-                                           .foregroundColor(Color.white)
-                                           .cornerRadius(6)
-                                   }
-                               }
-                       }.padding(.leading,40).padding(.trailing,40)
-                   }
-               }
-               
-           } //Ending body
+    
+    @State var userIsntRegistered: Bool = false
+    @State var forgotPassword: Bool = false
+    
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        GeometryReader{ geo in
+            VStack(alignment: .leading, spacing:50){
+                VStack(alignment:.leading){
+                    Text("Welcome,")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    Text("sign in to continue!")
+                        .font(.title)
+                        .foregroundColor(Color.gray)
+                        .fontWeight(.semibold)
+                }.padding(.top,100).padding(.leading,40)
+                VStack(alignment: .trailing){
+                    TextField("Enter Username", text: self.$username)
+                        .padding(.all)
+                        .frame(height:50)
+                        .background(Color.init(white: 0.95))
+                        .cornerRadius(6)
+                        .font(.system(size: 13, weight:.bold))
+                    SecureField("Enter Password", text: self.$password)                  .padding(.all)
+                        .frame(height:50)
+                        .background(Color.init(white: 0.95))
+                        .cornerRadius(6)
+                        .font(.system(size: 13, weight:.bold)).foregroundColor(Color.primary)
+                    Button(action: {
+                        print("Implement Forgot Password!")
+                        self.forgotPassword.toggle()
+                    }) {
+                        Text("Forgot Password?").font(.system(size: 14)).fontWeight(.semibold)
+                    }.sheet(isPresented: self.$forgotPassword){
+                        ForgotPassword()
+                    }
+                    Spacer()
+                    
+                    HStack(alignment: .center) {
+                        Button(action: {
+                            self.userIsntRegistered.toggle()
+                        }) {
+                            Text("Sign Up")
+                        }.sheet(isPresented: self.$userIsntRegistered){
+                            SignUp()
+                        }
+                        
+                        
+                        Spacer()
+                        Button(action: {
+                            print("Implement next button")
+                        }) {
+                            Text("Next")
+                                .font(.system(size: 15, weight:.bold))
+                                .frame(minWidth: 0, maxWidth: 100, minHeight: 45)
+                                .background(Color.init(red: 14/255, green: 219/255, blue: 139/255))
+                                .foregroundColor(Color.white)
+                                .cornerRadius(6)
+                        }
+                    }
+                }.padding(.leading,40).padding(.trailing,40)
+            }
+        }
+        
+    } //Ending body
 }
+
+//func goToSignUpScreen(){
+//    if let window = UIApplication.shared.windows.first {
+//        window.rootViewController = UIHostingController(rootView: SignUp())
+//        window.makeKeyAndVisible()
+//    }
+//}
 
 struct Welcome_Previews: PreviewProvider {
     static var previews: some View {
