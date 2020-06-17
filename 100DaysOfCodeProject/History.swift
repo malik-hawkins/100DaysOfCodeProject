1f4a7236f37f4bd3016ba060cac85b41eb98ba68
Malik Hawkins <hawkinsjm94@gmail.com>
2020-06-17 06:10:56 -0400

    First iteration of the home screen

---
+++ 100DaysOfCodeProject/History.swift
@@ -0,0 +1,21 @@
+//
+//  History.swift
+//  100DaysOfCodeProject
+//
+//  Created by Malik Hawkins on 6/17/20.
+//  Copyright © 2020 Malik Hawkins. All rights reserved.
+//
+
+import SwiftUI
+
+struct History: View {
+    var body: some View {
+        Text("History Page")
+    }
+}
+
+struct History_Previews: PreviewProvider {
+    static var previews: some View {
+        History()
+    }
+}
--- 100DaysOfCodeProject/Home.swift
+++ 100DaysOfCodeProject/Home.swift
@@ -11,35 +11,111 @@ import Firebase
 import FirebaseAuth
 
 struct Home: View {
+    
+    @State var profileClicked: Bool = false
+    @State var historyClicked: Bool = false
+    @State var settingsClicked: Bool = false
+    @State private var rentalDate = Date()
+    
     var body: some View {
-        VStack(){
-            Text("Welcome to the App").padding()
-            Button(action: {
+        GeometryReader { geo in
+            VStack(){
+                Image("homeScreenPic")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.top)
+                    .scaledToFit()
+                Button(action: {
+                    // This should take user to a calendar to select receive/return dates
+                    print("Implement select your dates button")
+                }) {
+                    Text("Select your dates to fly!")  .font(.system(size: 15, weight:.bold))
+                        .frame(minWidth: 0, maxWidth: 250, minHeight: 40)
+                        .background(Color.white)
+                        .foregroundColor(Color.gray)
+                        .cornerRadius(6)
+                        .padding(.top, -30)
+                }
+                Spacer()
+                HStack(){
+                    Button(action: {
+                        print("Implement profile button")
+                        self.profileClicked.toggle()
+                    }) {
+                        VStack(){
+                            Image(systemName: "person")
+                                .font(.system(size: 50))
+                                .foregroundColor(Color.black)
+                            Text("Profile")
+                            .font(.system(size: 15, weight:.bold))
+                            .foregroundColor(Color.black)
+                        }.sheet(isPresented: self.$profileClicked){
+                            Profile()
+                        }
+                    }
+                    Spacer()
+                    Button(action: {
+                        print("Implement history button")
+                        self.historyClicked.toggle()
+                    }) {
+                        VStack(){
+                            Image(systemName: "arrowshape.turn.up.left")
+                            .font(.system(size: 50))
+                            .foregroundColor(Color.black)
+                            Text("History")
+                            .font(.system(size: 15, weight:.bold))
+                            .foregroundColor(Color.black)
+                        }.sheet(isPresented: self.$historyClicked){
+                            History()
+                        }
+                        
+                    }
+                    Spacer()
+                    Button(action: {
+                        print("Implement settings button")
+                        self.settingsClicked.toggle()
+                    }) {
+                        VStack(){
+                            Image(systemName: "gear")
+                            .font(.system(size: 50))
+                            .foregroundColor(Color.black)
+                            Text("Settings")
+                            .font(.system(size: 15, weight:.bold))
+                            .foregroundColor(Color.black)
+                        }.sheet(isPresented: self.$settingsClicked){
+                            Settings()
+                        }
+                        
+                    }
+                }.padding()
                 
-                self.signOut()
-                
-            }) {
-                Text("Sign Out")
+                Spacer()
+                HStack(){
+                    Text("Popular Drones")
                     .font(.system(size: 15, weight:.bold))
-                    .frame(minWidth: 0, maxWidth: .greatestFiniteMagnitude, minHeight: 50)
-                    .background(Color.init(red: 14/255, green: 219/255, blue: 139/255))
-                    .foregroundColor(Color.white)
-                    .cornerRadius(6)
-            }.padding(.horizontal)
+                    Spacer()
+                    Button(action: {
+                        print("Implement view all")
+                    }) {
+                        Text("View All")
+                        .font(.system(size: 15, weight:.bold))
+                        .foregroundColor(Color.init(red: 14/255, green: 219/255, blue: 139/255))
+                    }
+                }.padding()
+                
+                ScrollView(.horizontal, showsIndicators: false){
+                    HStack(alignment: .center, spacing: 10) {
+                        ForEach(0..<3) {_ in
+                            PopularDronesCard()
+                        }
+                    }
+                }.padding(.horizontal)
+                                
+                Spacer()
+                
+            }.background(Color.init(white: 0.95)).edgesIgnoringSafeArea(.bottom)
         }
         
     } // Ending body
-    
-    func signOut(){
-        let firebaseAuth = Auth.auth()
-        do {
-            try firebaseAuth.signOut()
-            print("user signed out successfully")
-            goToWelcomePage()
-        } catch let signOutError as NSError {
-            print("Error signing out: %@", signOutError)
-        }
-    }
     
 } // Ending view
 
